c14021486ab4d13d97f42a11f9adc335c60ec11b
李昀宸 <liyunchen@liyunchendeAir.home>
2023-09-29 02:09:27 +0800

    feat: uri usage

--- app/search/result/index.tsx
+++ app/search/result/index.tsx
@@ -1,11 +1,11 @@
-import { Image } from "expo-image";
-import { router } from "expo-router";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
-import { styled } from "styled-components/native";
+import { Image } from 'expo-image';
+import { router } from 'expo-router';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { styled } from 'styled-components/native';
 
-import Stack from "@/components/stack";
-import FlashList from "@/components/util/FlashList";
-import { useResults } from "@/contexts/useResults";
+import Stack from '@/components/stack';
+import FlashList from '@/components/util/FlashList';
+import { useResults } from '@/contexts/useResults';
 
 const FOLDER_SIZE = 150;
 const TRADEMARK_CONTAINER_BORDER_RADIUS = 10;
@@ -30,27 +30,27 @@ const ResultContainer = styled.View`
 export default function Page() {
   const {
     results = [
-      [1, "李昀宸sdfghjiugghnmkjhg", ""],
-      [2, "林立起", ""],
-      [3, "陳傑同", ""],
-      [4, "賴奕蓁", ""],
-      [5, "劉宥儀", ""],
-      [6, "鄭安芸", ""],
+      [1, '李昀宸sdfghjiugghnmkjhg', ''],
+      [2, '林立起', ''],
+      [3, '陳傑同', ''],
+      [4, '賴奕蓁', ''],
+      [5, '劉宥儀', ''],
+      [6, '鄭安芸', ''],
     ],
   } = useResults();
 
   return (
     <Stack>
-      <View style={{ width: "90%", height: "100%", position: "relative" }}>
+      <View style={{ width: '90%', height: '100%', position: 'relative' }}>
         <FlashList<typeof results>
           data={results}
-          items={({ item: [id, name], index }) => (
+          items={({ item: [id, name, url], index }) => (
             <TouchableOpacity
-              style={styles["Flashlist.renderItem"]}
+              style={styles['Flashlist.renderItem']}
               key={index}
               onPress={() =>
                 router.push({
-                  pathname: "/search/result/detail/[id]",
+                  pathname: '/search/result/detail/[id]',
                   params: { id },
                 })
               }
@@ -61,12 +61,12 @@ export default function Page() {
                     flex: 1,
                     width: 150,
                     height: 130,
-                    backgroundColor: "#0553",
-                    overflow: "hidden",
+                    backgroundColor: '#0553',
+                    overflow: 'hidden',
                   }}
                   source={
-                    // `http://140.112.106.88:8082/${url}`
-                    require("@/assets/figure.png")
+                    { uri: `http://140.112.106.88:8082/${url}` }
+                    // require("@/assets/figure.png")
                   }
                   // placeholder={blurhash}
                   contentFit="contain"
@@ -84,8 +84,8 @@ export default function Page() {
 }
 
 const styles = StyleSheet.create({
-  "Flashlist.renderItem": {
-    shadowColor: "#000",
+  'Flashlist.renderItem': {
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
       height: 7,
@@ -93,10 +93,10 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.4,
     shadowRadius: 5,
     elevation: 6,
-    position: "relative",
+    position: 'relative',
     marginBottom: 20,
     borderTopLeftRadius: TRADEMARK_CONTAINER_BORDER_RADIUS,
     borderTopRightRadius: TRADEMARK_CONTAINER_BORDER_RADIUS,
-    backgroundColor: "white",
+    backgroundColor: 'white',
   },
 });
